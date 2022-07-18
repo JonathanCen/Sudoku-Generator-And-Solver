1ddbd6a7f62b03a778daba048e5718f278f8f23f
JonathanCen <jcen3@binghamton.edu>
2022-07-18 19:30:00 -0400

    Updated files

--- src/frontend/src/components/ContactInfo.js
+++ src/frontend/src/components/ContactInfo.js
@@ -3,31 +3,32 @@ import Stack from "@mui/material/Stack";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Typography from "@mui/material/Typography";
 
-
 function ContactInfo(props) {
-    return (
-        <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{
-                width: "500px"
-            }}
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        width: "500px",
+      }}
+    >
+      <Typography variant="subtitle1">
+        <a id="github-link" href="https://github.com/" target="_blank">
+          Source Code <GitHubIcon fontSize="small" />
+        </a>
+      </Typography>
+      <Typography component="overline" variant="overline">
+        <a
+          id="personal-page-link"
+          href="https://jonathancen.me"
+          target="_blank"
         >
-            <Typography
-                variant="subtitle1"
-            >
-                <a id="github-link" href="https://github.com/">Source Code <GitHubIcon fontSize="small" /></a>
-            </Typography>
-            <Typography
-                component="overline"
-                variant="overline"
-            >
-                <a id="personal-page-link" href="https://google.com/">Jonathan Cen</a>
-            </Typography>
-        </Stack >
-
-    )
+          Jonathan Cen
+        </a>
+      </Typography>
+    </Stack>
+  );
 }
 
 export default ContactInfo;
